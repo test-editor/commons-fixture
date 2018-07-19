1d7fc610b08f10257b23bc3b1d5bc008855c2c99
kbayraktar <kbayraktar@web.de>
2018-07-19 10:47:12 +0200

    NameGenerator added

---
+++ src/test/java/org/testeditor/fixture/commons/text/generate/RandomNameGeneratorTest.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2012 - 2018 Signal Iduna Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Signal Iduna Corporation - initial API and implementation
+ * akquinet AG
+ * itemis AG
+ *******************************************************************************/
+
+package org.testeditor.fixture.commons.text.generate;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class RandomNameGeneratorTest {
+
+    @Test
+    void generateNameTest() {
+        String randomName = RandomNameGenerator.getRandomName(8);
+        Assert.assertTrue(randomName.length() > 0 && randomName.length() <= 8);
+
+    }
+
+}
+
+
